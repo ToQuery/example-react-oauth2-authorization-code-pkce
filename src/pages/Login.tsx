7412af88f749ecf31af4fa6459413a77e967125d
ToQuery <toquery@qq.com>
2024-02-20 14:17:53 +0800

    upgrade

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,10 +7,32 @@ export const Login = () => {
     debugger;
 
     switch (auth.activeNavigator) {
+        case "signinRedirect":
+            console.log("signinRedirect...");
+            // return <div>Redirecting...</div>;
+            break;
+        case "signinResourceOwnerCredentials":
+            console.log("signinResourceOwnerCredentials...");
+            // return <div>signinResourceOwnerCredentials...</div>;
+            break;
+        case "signinPopup":
+            console.log("signinPopup...");
+            // return <div>signinPopup...</div>;
+            break;
         case "signinSilent":
+            console.log("signinSilent...");
             return <div>Signing you in...</div>;
         case "signoutRedirect":
+            console.log("signoutRedirect...");
             return <div>Signing you out...</div>;
+        case "signoutPopup":
+            console.log("signoutPopup...");
+            // return <div>signoutPopup...</div>;
+            break;
+        case "signoutSilent":
+            console.log("signoutSilent...");
+            // return <div>signoutSilent...</div>;
+            break;
     }
 
     if (auth.isLoading) {
